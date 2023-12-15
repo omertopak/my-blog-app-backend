5d8fafc887b1de59205363789f18ffbc6a510c14
omer <omertopak1@gmail.com>
2023-12-15 16:38:28 +0000

    up

--- src/models/comment.js
+++ src/models/comment.js
@@ -8,15 +8,16 @@ const { mongoose } = require('../configs/dbConnection')
 const CommentSchema = new mongoose.Schema({
 
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref:'User'
+        type: String,
+        trim: true,
+        required: true,
     },
     content: {
         type: String,
         trim: true,
         required: true,
     },
-
+    
 
 }, { collection: 'comments', timestamps: {
     createdAt: 'time_stamp', 
